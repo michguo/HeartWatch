a56ada7c406fb3b13df698fe1373d31aef851abd
Cliff Weitzman <cliffweitzman@gmail.com>
2015-02-21 15:12:27 -0800

    first commit

---
+++ HeartWatch/input.swift
@@ -0,0 +1,13 @@
+//
+//  input.swift
+//  HeartWatch
+//
+//  Created by Cliff Weitzman on 2/21/15.
+//  Copyright (c) 2015 Cliff Weitzman. All rights reserved.
+//
+
+import Cocoa
+
+class input: UIViewController {
+
+}
