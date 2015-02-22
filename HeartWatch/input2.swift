6200e216cfbd6acb05682febb0bc8394de7c2b95
Taylor Pullinger <taylor_pullinger@brown.edu>
2015-02-21 21:26:47 -0800

    Taylor's first client commit

--- HeartWatch/input2.swift
+++ HeartWatch/input2.swift
@@ -8,8 +8,35 @@
 
 import UIKit
 
-class input2: UIViewController {
 
+var height_in = 69
+var weight_lb = 150
+
+class input2: UIViewController {
+    //@IBOutlet weak var age: UITextField!
+    @IBOutlet weak var height: UITextField!
+    @IBOutlet weak var weight: UITextField!
+
+    @IBOutlet weak var age: UITextField!
+    var age_yrs = 20
+    //var age2_yrs = age.text
+    
+    var age_yrs = age.text.toInt()
+    
+    
+    
+    
+    
+    
+    @IBOutlet weak var contact1: UITextField!
+    @IBOutlet weak var contact2: UITextField!
+    @IBOutlet weak var contact3: UITextField!
+    @IBOutlet weak var contact4: UITextField!
+    @IBOutlet weak var contact5: UITextField!
+    @IBOutlet weak var contact6: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
